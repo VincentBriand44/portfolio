ea20ce9494a194d2863dd96f8c08b58044d4a355
Kanon <canon321@hotmail.fr>
2022-05-01 10:34:19 +0200

    main

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-gray-700 h-screen w-screen text-white flex justify-center items-center">
+      <p>Test</p>
+    </div>
+  );
+}
+
+export default App;
