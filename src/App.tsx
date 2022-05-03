3c10a87c7d2da7ab8b9f00b315ad7b88ff536f28
Kanon <canon321@hotmail.fr>
2022-05-03 09:28:33 +0200

    language.childs[i].file

--- src/App.tsx
+++ src/App.tsx
@@ -26,12 +26,28 @@ function App() {
       file: "js.png",
       childs: [
         {
+          title: "Node JS",
+          file: "node.png",
+        },
+        {
           title: "React",
           file: "react.png",
         },
         {
           title: "Vite",
           file: "vite.png",
+        },
+        {
+          title: " ",
+          file: " ",
+        },
+        {
+          title: " ",
+          file: " ",
+        },
+        {
+          title: " ",
+          file: " ",
         },
       ],
     },
@@ -73,13 +89,11 @@ function App() {
       </header>
       <main>
         <div className="flex items-center justify-center h-screen pb-24 md:pb-40">
-          <div
-            className={`grid grid-cols-1 gap-2 lg:gap-10 lg:grid-cols-${languages.length}`}
-          >
+          <div className={`grid grid-cols-1 gap-2 lg:gap-10 lg:grid-cols-5`}>
             {languages.map((language) => (
               <div className="flex items-center lg:flex-col">
                 <div className="flex flex-col mb-2 text-center">
-                  <div className="flex items-center justify-center w-24 h-24 bg-black/[.3] rounded-full lg:mb-2 lg:w-32 lg:h-32 select-none">
+                  <div className="flex items-center justify-center w-24 h-24 bg-black/[.3] rounded-full lg:w-32 lg:h-32 select-none">
                     <img
                       className="w-16 h-16 lg:w-20 lg:h-20"
                       src={`/src/img/${language.file}`}
@@ -88,13 +102,32 @@ function App() {
                   </div>
                   {/* <p>{language.title}</p> */}
                 </div>
-                <div className="flex items-center content-around">
-                  {language.childs.map((child) => (
-                    <div className="flex flex-col items-center w-24">
-                      <div className="w-8 h-8 bg-white rounded-full select-none"></div>
-                      <p className="text-xs">{child.title}</p>
-                    </div>
-                  ))}
+                <div className="flex flex-wrap justify-center w-24 ml-2 lg:ml-0 lg:w-32 last:mt-0">
+                  {language.childs.map((child, i) => {
+                    let marginTop = "0";
+                    if ((i + 1) % 4 === 0 && language.childs.length % 4 === 0) {
+                      marginTop = "10px";
+                    } else if (
+                      (i + 2) % 3 === 0 &&
+                      i !== language.childs.length - 1
+                    ) {
+                      marginTop = "10px";
+                    }
+                    return (
+                      <div
+                        className="flex flex-col items-center w-1/3"
+                        style={{ marginTop }}
+                      >
+                        <div className="flex items-center justify-center w-8 h-8 text-center bg-white rounded-full select-none">
+                          <img
+                            src={`/src/img/${language.childs[i].file}`}
+                            className="w-6 h-6"
+                            draggable="false"
+                          />
+                        </div>
+                      </div>
+                    );
+                  })}
                 </div>
               </div>
             ))}
