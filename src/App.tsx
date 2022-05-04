c11d064028f675188bbbda11c151b189bde4bcb9
Kanon <canon321@hotmail.fr>
2022-05-04 23:31:17 +0200

    big commit

--- src/App.tsx
+++ src/App.tsx
@@ -34,20 +34,8 @@ function App() {
           file: "react.png",
         },
         {
-          title: "Vite",
-          file: "vite.png",
-        },
-        {
-          title: " ",
-          file: " ",
-        },
-        {
-          title: " ",
-          file: " ",
-        },
-        {
-          title: " ",
-          file: " ",
+          title: "React",
+          file: "react.png",
         },
       ],
     },
@@ -60,6 +48,49 @@ function App() {
       title: "SQL",
       file: "sql.png",
       childs: [],
+    },
+  ];
+
+  const projects = [
+    {
+      title: "Projet 1",
+      file: "html.png",
+      link: "#",
+    },
+    {
+      title: "Projet 1",
+      file: "html.png",
+      link: "#",
+    },
+    {
+      title: "Projet 1",
+      file: "html.png",
+      link: "#",
+    },
+    {
+      title: "Projet 1",
+      file: "html.png",
+      link: "#",
+    },
+    {
+      title: "Projet 1",
+      file: "html.png",
+      link: "#",
+    },
+    {
+      title: "Projet 1",
+      file: "html.png",
+      link: "#",
+    },
+    {
+      title: "Projet 1",
+      file: "html.png",
+      link: "#",
+    },
+    {
+      title: "Projet 1",
+      file: "html.png",
+      link: "#",
     },
   ];
 
@@ -105,7 +136,10 @@ function App() {
                 <div className="flex flex-wrap justify-center w-24 ml-2 lg:ml-0 lg:w-32 last:mt-0">
                   {language.childs.map((child, i) => {
                     let marginTop = "0";
-                    if ((i + 1) % 4 === 0 && language.childs.length % 4 === 0) {
+                    if (
+                      i === language.childs.length - 1 &&
+                      (language.childs.length - 1) % 3 === 0
+                    ) {
                       marginTop = "10px";
                     } else if (
                       (i + 2) % 3 === 0 &&
@@ -133,7 +167,64 @@ function App() {
             ))}
           </div>
         </div>
-        <div className="h-screen"></div>
+        <div className="flex items-center justify-center h-screen">
+          <div className={`grid grid-cols-1 gap-2 lg:gap-10 lg:grid-cols-5`}>
+            {projects.map((project) => (
+              <div className="flex items-center lg:flex-col">
+                <div className="flex flex-col mb-2 text-center">
+                  <div className="flex items-center justify-center w-24 h-24 bg-black/[.3] rounded-full lg:w-32 lg:h-32 select-none">
+                    <img
+                      className="w-16 h-16 lg:w-20 lg:h-20"
+                      src={`/src/img/${project.file}`}
+                      draggable="false"
+                    />
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center h-screen text-slate-800">
+          <label htmlFor="name" className="text-slate-200">
+            Prénom/NOM
+          </label>
+          <input
+            type="text"
+            name="name"
+            id=""
+            placeholder="Prénom NOM"
+            className="w-full h-10 max-w-2xl p-2 mb-8 bg-gray-200 rounded-lg"
+          />
+          <label htmlFor="email" className="text-slate-200">
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            placeholder="exemple@email.fr"
+            className="w-full h-10 max-w-2xl p-2 mb-8 bg-gray-200 rounded-lg"
+          />
+          <label htmlFor="subject" className="text-slate-200">
+            Sujet
+          </label>
+          <input
+            type="text"
+            name="subject"
+            placeholder="Mon sujet"
+            className="w-full h-10 max-w-2xl p-2 mb-8 bg-gray-200 rounded-lg"
+          />
+          <label htmlFor="message" className="text-slate-200">
+            Message
+          </label>
+          <textarea
+            name="message"
+            className="w-full h-32 max-w-2xl p-2 mb-8 bg-gray-200 rounded-lg resize-y"
+            placeholder="Mon message"
+          ></textarea>
+          <button className="px-8 py-2 font-medium border-2 border-gray-600 rounded-md text-slate-200 hover:bg-slate-200 hover:text-gray-800 hover:border-gray-800">
+            Envoyer
+          </button>
+        </div>
       </main>
       <footer className="flex justify-center">
         <div className="fixed grid w-screen max-w-2xl grid-cols-4 text-xs pointer-events-none select-none bottom-4 md:text-base md:bottom-16">
