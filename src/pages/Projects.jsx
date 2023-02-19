06b78550685d6e3814ebc94094706826142bb2c3
VincentBriand44 <vincent-briand_student2022@wilder.school>
2023-02-19 17:24:42 +0100

    double map

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -11,20 +11,43 @@ const Projects = () => {
 
   return (
     <div
-      className='flex items-center justify-center h-full page snap-center'
+      className='flex items-center justify-center w-full h-full snap-center'
       id='projects'
     >
-      <div className={`grid grid-cols-2 gap-2 lg:gap-10 lg:grid-cols-3`}>
+      <a href='#projects__1' title='Next'>
+        Précédent
+      </a>
+      <div className='relative flex items-center justify-center h-full overflow-x-scroll scroll-smooth snap-x snap-mandatory snap-center'>
         {projects?.map(
-          (project, index) =>
-            index < 9 && (
-              <ProjectsBox project={project} setPopup={setPopup} key={index} />
-            )
-        )}
-        {popup?.title && (
-          <ProjectPopup popup={popup} setPopup={setPopup} reset={reset} />
+          (_, i) =>
+            console.log(i) ||
+            (i % 9 === 0 && (
+              <div
+                className='grid w-full min-w-full grid-cols-2 gap-2 overflow-hidden origin-center scale-100 lg:gap-10 lg:grid-cols-3'
+                key={i}
+                id={`projects__${i / 9 + 1}`}
+              >
+                {/*
+                    console.log(`${i}: ${index}`) ||
+                    (index + i >= i * 2 && index + i <= i &&
+                 */}
+                {projects.map((project, index) => (
+                  <ProjectsBox
+                    project={project}
+                    setPopup={setPopup}
+                    key={index}
+                  />
+                ))}
+              </div>
+            ))
         )}
       </div>
+      {popup?.title && (
+        <ProjectPopup popup={popup} setPopup={setPopup} reset={reset} />
+      )}
+      <a href='#projects__2' title='Next'>
+        Suivant
+      </a>
     </div>
   )
 }
